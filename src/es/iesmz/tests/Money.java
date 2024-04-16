ca72db4b1c7b7a24208736aab9d6caa0521cdf6e
Mael Grandguillot Milvaques <dam_gmm@informatica.mz>
2024-04-16 12:36:24 +0200

    Clases iniciales

---
+++ src/es/iesmz/tests/Money.java
@@ -0,0 +1,14 @@
+package es.iesmz.tests;
+
+public class Money {
+    private final double K_EURtoUSD = 1.18798;
+    private final double K_USDtoEUR = 0.841815;
+    private final double K_EURtoGBP = 0.857839;
+    private final double K_GBPtoEUR = 1.165826;
+
+    public Money() {}
+
+    public float change(TipoMoneda eur, TipoMoneda usd, float v) {
+        return -1
+    }
+}
--- src/es/iesmz/tests/MoneyTest.java
+++ src/es/iesmz/tests/MoneyTest.java
@@ -8,54 +8,63 @@ public class MoneyTest {
     @Test
     public void test1(){
         float esperado = 28.37f;
-        assertEquals(esperado, Money.change(TipoMoneda.EUR, TipoMoneda.USD, 23.88f));
+        Money dinero = new Money();
+        assertEquals(esperado, dinero.change(TipoMoneda.EUR, TipoMoneda.USD, 23.88f));
     }
 
     @Test
     public void test2(){
         float esperado = 1165.83f;
-        assertEquals(esperado, Money.change(TipoMoneda.GBP, TipoMoneda.EUR, 1000.0f));
+        Money dinero = new Money();
+        assertEquals(esperado, dinero.change(TipoMoneda.GBP, TipoMoneda.EUR, 1000.0f));
     }
 
     @Test
     public void test3(){
         float esperado = 201.21f;
-        assertEquals(esperado, Money.change(TipoMoneda.EUR, TipoMoneda.GBP, 234.56f));
+        Money dinero = new Money();
+        assertEquals(esperado, dinero.change(TipoMoneda.EUR, TipoMoneda.GBP, 234.56f));
     }
 
     @Test
     public void test4(){
         float esperado = 37.51f;
-        assertEquals(esperado, Money.change(TipoMoneda.USD, TipoMoneda.EUR, 44.56f));
+        Money dinero = new Money();
+        assertEquals(esperado, dinero.change(TipoMoneda.USD, TipoMoneda.EUR, 44.56f));
     }
 
     @Test
     public void test5(){
         float esperado = 138.49f;
-        assertEquals(esperado, Money.change(TipoMoneda.GBP, TipoMoneda.USD, 100.0f));
+        Money dinero = new Money();
+        assertEquals(esperado, dinero.change(TipoMoneda.GBP, TipoMoneda.USD, 100.0f));
     }
 
     @Test
     public void test6(){
         float esperado = 722.14f;
-        assertEquals(esperado, Money.change(TipoMoneda.USD, TipoMoneda.GBP, 1000.0f));
+        Money dinero = new Money();
+        assertEquals(esperado, dinero.change(TipoMoneda.USD, TipoMoneda.GBP, 1000.0f));
     }
 
     @Test
     public void test7(){
         float esperado = -1f;
-        assertEquals(esperado, Money.change(TipoMoneda.PTS, TipoMoneda.EUR, -1f));
+        Money dinero = new Money();
+        assertEquals(esperado, dinero.change(TipoMoneda.PTS, TipoMoneda.EUR, -1f));
     }
 
     @Test
     public void test8(){
         float esperado = -1f;
-        assertEquals(esperado, Money.change(TipoMoneda.EUR, TipoMoneda.PTS, -1f));
+        Money dinero = new Money();
+        assertEquals(esperado, dinero.change(TipoMoneda.EUR, TipoMoneda.PTS, -1f));
     }
 
     @Test
     public void test9(){
         float esperado = -1f;
-        assertEquals(esperado, Money.change(TipoMoneda.USD, TipoMoneda.EUR, -1f));
+        Money dinero = new Money();
+        assertEquals(esperado, dinero.change(TipoMoneda.USD, TipoMoneda.EUR, -1f));
     }
 }
